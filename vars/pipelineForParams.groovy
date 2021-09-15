a1f508996839dccee759762d26f1d29daf83176b
soch <soch@gft,com>
2021-09-15 12:58:00 +0200

    Initial commit

--- vars/pipelineForParams.groovy
+++ vars/pipelineForParams.groovy
@@ -2,21 +2,8 @@
 
 import com.demo.ParamsPipeline
 
-def call(config){
-    pipeline{
-        // Se ejecuta en cualquier agente libre
-        agent any
-
-        stages {
-            stage('Params') {
-                steps {
-                    new ParamsPipeline()
-                            .withParm1(params.environment)
-                            .withParm2(params.dryRun)
-                            .withParm3(params.verifier)
-                            .execute()
-                }
-            }
-        }
-    }
-}
+new ParamsPipeline()
+        .withParm1(params.environment)
+        .withParm2(params.dryRun)
+        .withParm3(params.verifier)
+        .execute()
